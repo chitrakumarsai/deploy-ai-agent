20755405386b50abb1d1948d0da6f68fd6f699e3
Chitra Kumar Sai <chitrakumarsai@gmail.com>
2025-06-20 17:32:04 -0500

    removing api key for testing again

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,9 +2,6 @@ from fastapi import FastAPI
 import os
 
 app = FastAPI()
-API_KEY = os.environ.get("API_KEY")
-if not API_KEY:
-    raise NotImplementedError(" API_KEY environment variable is not set. Please set it before running the application.")
 @app.get("/")
 def read_index():
     return {"message": "Welcome to the FastAPI application! This is the index page."}   
